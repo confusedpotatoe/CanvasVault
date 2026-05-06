32d4570d641c6165a52efb034ae1165ed8c42273
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-06 10:03:38 +0200

    added methodes in artworkRepository

--- CanvasVault.Infrastructure/Repositories/ArtworkRepository.cs
+++ CanvasVault.Infrastructure/Repositories/ArtworkRepository.cs
@@ -1,10 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CanvasVault.Infrastructure.Repositories
+﻿namespace CanvasVault.Infrastructure.Repositories
 {
-	internal class ArtworkRepository
+	public class ArtworkRepository
 	{
+		private readonly CanvasVaultDbContext _context;
+
+		public ArtworkRepository(CanvasVaultDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<Artwork>> GetAllAsync()
+		{
+			return await _context.Artworks.ToListAsync();
+		}
+
+		public async Task<Artwork> GetByIdAsync(int id)
+		{
+			return await _context.Artworks.FindAsync(id);
+		}
+
+		public async Task AddAsync(Artwork artwork)
+		{
+			await _context.Artworks.AddAsync(artwork);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task UpdateAsync(Artwork artwork)
+		{
+			_context.Artworks.Update(artwork);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			var artwork = await _context.Artworks.FindAsync(id);
+			if (artwork != null)
+			{
+				_context.Artworks.Remove(artwork);
+				await _context.SaveChangesAsync();
+			}
+		}
 	}
 }
