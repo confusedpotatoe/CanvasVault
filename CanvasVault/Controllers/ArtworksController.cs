9d342602ac9798dff4a1b644ea1813242bd94f89
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-06 12:47:10 +0200

    added mediator artwork controller

---
+++ CanvasVault/Controllers/ArtworksController.cs
@@ -0,0 +1,26 @@
+﻿using CanvasVault.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CanvasVault.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ArtworksController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public ArtworksController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllArtworks()
+		{
+			var query = new GetAllArtworksQuery();
+			var artworks = await _mediator.Send(query);
+			return Ok(artworks);
+		}
+	}
+}
