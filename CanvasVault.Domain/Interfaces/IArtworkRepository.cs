f9f4870ecf49c6a05efd7a95ed7c9e52ed99d9a5
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-06 09:57:46 +0200

    added Asynchronous Methods for IArtwork

--- CanvasVault.Domain/Interfaces/IArtworkRepository.cs
+++ CanvasVault.Domain/Interfaces/IArtworkRepository.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CanvasVault.Domain.Interfaces
+﻿namespace CanvasVault.Domain.Interfaces
 {
-	internal interface IArtworkRepository
+	public interface IArtworkRepository
 	{
+		// Asynchronous method to retrieve all artworks from the database
+		// Returns a task that represents the asynchronous operation, containing an enumerable collection of Artwork objects.
+		Task<IEnumerable<Artwork>> GetAllArtworksAsync();
+
+		// Asynchronous method to retrieve a specific artwork by its unique identifier (Id)
+		Task<Artwork> GetByIdAsync(int id);
+
+		// Asynchronous method to add a new artwork to the database
+		Task AddAsync(Artwork artwork);
+
+		// Asynchronous method to update an existing artwork in the database
+		Task UpdateAsync(Artwork artwork);
+
+		// Asynchronous method to delete an artwork from the database by its unique identifier (Id)
+		Task DeleteAsync(int id);
 	}
+
 }
+
