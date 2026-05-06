a34b52f64407fde51824eb892681edc88f4aaa25
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-06 09:49:56 +0200

    added primary keys for collection

--- CanvasVault.Domain/Entities/Collection.cs
+++ CanvasVault.Domain/Entities/Collection.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CanvasVault.Domain.Entities
+﻿namespace CanvasVault.Domain.Entities
 {
-	internal class Collection
+	public class Collection
 	{
+		// Primary key for the Collection entity
+		// The Id property is an integer that uniquely identifies each collection in the database.
+		public int Id { get; set; }
+
+		// Name of the collection
+		public string Name { get; set; } = string.Empty;
+
+		// Optional description of the collection
+		public string? Description { get; set; }
+
+		// Navigation property to represent the relationship between Collection and Artwork
+		// A collection can contain multiple artworks, so we use a list to hold the related artworks.
+		public List<Artwork> Artworks { get; set; } = new List<Artwork>();
 	}
 }
