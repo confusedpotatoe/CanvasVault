a42336d37be2d5d9bfa07d5172fde7fe832bb04b
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-06 10:12:08 +0200

    added DB context

---
+++ CanvasVault.Infrastructure/CanvasVaultDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace CanvasVault.Infrastructure
+{
+	public class CanvasVaultDbContext
+	{
+		public CanvasVaultDbContext(DbContextOptions<CanvasVaultDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Artwork> Artworks { get; set; }
+		public DbSet<Collection> Collections { get; set; }
+	}
+}
