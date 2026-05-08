de9ea9993919059dc537dad53ad60732bc475154
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-08 15:08:44 +0200

    added GetAllCollection querie

---
+++ CanvasVault.Application/Queries/GetAllCollectionsQuery.cs
@@ -0,0 +1,26 @@
+﻿using CanvasVault.Domain.Entities;
+using CanvasVault.Domain.Interfaces;
+using MediatR;
+
+namespace CanvasVault.Application.Queries
+{
+	public class GetAllCollectionsQuery : IRequest<IEnumerable<Collection>>
+	{
+	}
+
+	public class GetAllCollectionsQueryHandler : IRequestHandler<GetAllCollectionsQuery, IEnumerable<Collection>>
+	{
+		private readonly ICollectionRepository _repository;
+
+		public GetAllCollectionsQueryHandler(ICollectionRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<IEnumerable<Collection>> Handle(GetAllCollectionsQuery request, CancellationToken cancellationToken)
+		{
+			return await _repository.GetAllAsync();
+		}
+
+	}
+}
--- CanvasVault/Controllers/CollectionsController.cs
+++ CanvasVault/Controllers/CollectionsController.cs
@@ -1,4 +1,5 @@
 ﻿using CanvasVault.Application.Commands;
+using CanvasVault.Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
