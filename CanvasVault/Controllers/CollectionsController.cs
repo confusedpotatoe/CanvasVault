9c68ea03e911eddbb83b780cab858e645199cbb4
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-08 15:03:24 +0200

    updated collectionController with crud

--- CanvasVault/Controllers/CollectionsController.cs
+++ CanvasVault/Controllers/CollectionsController.cs
@@ -6,7 +6,7 @@ namespace CanvasVault.API.Controllers
 {
 	[ApiController]
 	[Route("api/[controller]")]
-	public class CollectionsController : Controller
+	public class CollectionsController : ControllerBase
 	{
 		private readonly IMediator _mediator;
 
@@ -14,13 +14,41 @@ namespace CanvasVault.API.Controllers
 		{
 			_mediator = mediator;
 		}
+		[HttpGet]
+		public async Task<IActionResult> GetCollections()
+		{
+			// Implementation for fetching collections
+			var collections = await _mediator.Send(new GetAllCollectionsQuery());
+			return Ok(collections);
+		}
 
 		[HttpPost]
 		public async Task<IActionResult> CreateCollection([FromBody] CreateCollectionCommand command)
 		{
 			var collectionId = await _mediator.Send(command);
-			return Ok(new { id = collectionId });
+			return CreatedAtAction(nameof(GetCollections), new { id = collectionId }, new { id = collectionId });
 		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> UpdateCollection(int id, [FromBody] UpdateCollectionCommand command)
+		{
+			if (id != command.Id)
+			{
+				return BadRequest("ID in URL does not match ID in body.");
+			}
+			await _mediator.Send(command);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteCollection(int id)
+		{
+			var command = new DeleteCollectionCommand { Id = id };
+			await _mediator.Send(command);
+			return NoContent();
+		}
+
+
 
 	}
 }
