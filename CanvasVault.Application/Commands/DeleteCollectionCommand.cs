029e4e61e3cddad8ae24d99d8ec555e59e4e4ce5
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-08 14:57:07 +0200

    added collection delete controller

---
+++ CanvasVault.Application/Commands/DeleteCollectionCommand.cs
@@ -0,0 +1,25 @@
+﻿using CanvasVault.Domain.Interfaces;
+using MediatR;
+
+namespace CanvasVault.Application.Commands
+{
+	public class DeleteCollectionCommand : IRequest<Unit>
+	{
+		public int Id { get; set; }
+	}
+
+	public class DeleteCollectionCommandHandler : IRequestHandler<DeleteCollectionCommand, Unit>
+	{
+		private readonly ICollectionRepository _repository;
+		public DeleteCollectionCommandHandler(ICollectionRepository repository)
+		{
+			_repository = repository;
+		}
+		public async Task<Unit> Handle(DeleteCollectionCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.DeleteAsync(request.Id);
+			return Unit.Value;
+		}
+	}
+
+}
