acee9d5df26ccd4ed76271a18653d79c9dfd5a4e
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-11 12:46:20 +0200

    seperated query and queryhandler

--- CanvasVault.Application/Queries/GetAllArtworksQuery.cs
+++ CanvasVault.Application/Queries/GetAllArtworksQuery.cs
@@ -1,7 +1,4 @@
 ﻿using CanvasVault.Application.DTOs;
-using CanvasVault.Domain.Entities;
-using CanvasVault.Domain.Interfaces;
-using Mapster;
 using MediatR;
 
 namespace CanvasVault.Application.Queries
@@ -11,25 +8,5 @@ namespace CanvasVault.Application.Queries
 	public class GetAllArtworksQuery : IRequest<IEnumerable<ArtworkDto>>
 	{
 	}
-
-	//	The GetAllArtworksQueryHandler class is responsible for handling the GetAllArtworksQuery and retrieving all artworks from the database using the IArtworkRepository.
-	//	It implements the IRequestHandler interface from the MediatR library, which allows it to process the query and return the appropriate result.
-	public class GetAllArtworksQueryHandler : IRequestHandler<GetAllArtworksQuery, IEnumerable<Artwork>>
-	{
-		private readonly IArtworkRepository _artworkRepository;
-
-		//	Constructor that initializes the GetAllArtworksQueryHandler with a reference to the IArtworkRepository.
-		public GetAllArtworksQueryHandler(IArtworkRepository artworkRepository)
-		{
-			_artworkRepository = artworkRepository;
-		}
-
-		//	Asynchronous method that handles the GetAllArtworksQuery and retrieves all artworks from the database using the IArtworkRepository.
-		public async Task<IEnumerable<Artwork>> Handle(GetAllArtworksQuery request, CancellationToken cancellationToken)
-		{
-			var artworks = await _artworkRepository.GetAllAsync();
-
-			return artworks.Adapt<IEnumerable<ArtworkDto>>();
-		}
-	}
 }
+
---
+++ CanvasVault.Application/Queries/GetAllArtworksQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using CanvasVault.Application.DTOs;
+using CanvasVault.Domain.Interfaces;
+using Mapster;
+using MediatR;
+
+namespace CanvasVault.Application.Queries
+{
+	public class GetAllArtworksQueryHandler : IRequestHandler<GetAllArtworksQuery, IEnumerable<ArtworkDto>>
+	{
+		private readonly IArtworkRepository _artworkRepository;
+
+		public GetAllArtworksQueryHandler(IArtworkRepository artworkRepository)
+		{
+			_artworkRepository = artworkRepository;
+		}
+
+		public async Task<IEnumerable<ArtworkDto>> Handle(GetAllArtworksQuery request, CancellationToken cancellationToken)
+		{
+			var artworks = await _artworkRepository.GetAllAsync();
+			return artworks.Adapt<IEnumerable<ArtworkDto>>();
+		}
+	}
+}
