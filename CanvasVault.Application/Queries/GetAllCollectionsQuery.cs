1a572998ea0fd2f28905a451185b299827ba1320
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-11 12:54:09 +0200

    seperated collections query

--- CanvasVault.Application/Queries/GetAllCollectionsQuery.cs
+++ CanvasVault.Application/Queries/GetAllCollectionsQuery.cs
@@ -1,26 +1,9 @@
-﻿using CanvasVault.Domain.Entities;
-using CanvasVault.Domain.Interfaces;
+﻿using CanvasVault.Application.DTOs;
 using MediatR;
 
 namespace CanvasVault.Application.Queries
 {
-	public class GetAllCollectionsQuery : IRequest<IEnumerable<Collection>>
+	public class GetAllCollectionsQuery : IRequest<IEnumerable<CollectionDto>>
 	{
-	}
-
-	public class GetAllCollectionsQueryHandler : IRequestHandler<GetAllCollectionsQuery, IEnumerable<Collection>>
-	{
-		private readonly ICollectionRepository _repository;
-
-		public GetAllCollectionsQueryHandler(ICollectionRepository repository)
-		{
-			_repository = repository;
-		}
-
-		public async Task<IEnumerable<Collection>> Handle(GetAllCollectionsQuery request, CancellationToken cancellationToken)
-		{
-			return await _repository.GetAllAsync();
-		}
-
 	}
 }
---
+++ CanvasVault.Application/Queries/GetAllCollectionsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using CanvasVault.Application.DTOs;
+using CanvasVault.Domain.Interfaces;
+using Mapster;
+using MediatR;
+
+namespace CanvasVault.Application.Queries
+{
+	public class GetAllCollectionsQueryHandler : IRequestHandler<GetAllCollectionsQuery, IEnumerable<CollectionDto>>
+	{
+		private readonly ICollectionRepository _repository;
+
+		public GetAllCollectionsQueryHandler(ICollectionRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<IEnumerable<CollectionDto>> Handle(GetAllCollectionsQuery request, CancellationToken cancellationToken)
+		{
+			var collections = await _repository.GetAllAsync();
+			return collections.Adapt<IEnumerable<CollectionDto>>();
+		}
+
+	}
+}
