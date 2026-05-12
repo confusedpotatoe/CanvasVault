11a19213f0bf131c739ce9cef083cb5d019b8035
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-12 10:29:29 +0200

    added record login commands

---
+++ CanvasVault.Application/Commands/LoginCommand.cs
@@ -0,0 +1,7 @@
+﻿using CanvasVault.Application.DTOs;
+using MediatR;
+
+namespace CanvasVault.Application.Commands
+{
+	public record LoginCommand(string Username, string Password) : IRequest<LoginResponseDto>;
+}
