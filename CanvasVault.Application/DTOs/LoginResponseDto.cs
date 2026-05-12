8b0cae5f755ab2bdd77cedc7fc8fdf53318451d5
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-12 10:21:48 +0200

    added LoginresponsDTO

---
+++ CanvasVault.Application/DTOs/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CanvasVault.Application.DTOs
+{
+	public class LoginResponseDto
+	{
+		public string Username { get; set; } = string.Empty;
+		public string Token { get; set; } = string.Empty;
+
+		public string Password { get; set; } = string.Empty; // This is not recommended in a real application, but included here for demonstration purposes
+	}
+}
