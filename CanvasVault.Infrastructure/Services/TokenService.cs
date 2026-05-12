d9b9027fd7644423a847eb00b90bc6fa8b4d808e
confusedpotatoe <milo.arvaa@gmail.com>
2026-05-12 10:17:00 +0200

    added tokenservice for jwt

---
+++ CanvasVault.Infrastructure/Services/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using CanvasVault.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CanvasVault.Infrastructure.Services
+{
+	public class TokenService : ITokenService
+	{
+		private readonly IConfiguration _config;
+		public TokenService(IConfiguration config) => _config = config;
+
+		public string CreateToken(string username, List<string> roles)
+		{
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Name, username)
+			};
+			foreach (var role in roles)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, role));
+			}
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var tokenDescriptor = new SecurityTokenDescriptor(
+				Subject = new ClaimsIdentity(claims),
+				Expires = DateTime.Now.AddDays(7),
+				SigningCredentials = creds,
+				Issuer = _config["Jwt:Issuer"],
+				Audience = _config["Jwt:Audience"]);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+		}
+
+	}
+}
